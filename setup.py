d86a19204e96d9afe97e5998a96cf58867780e01
aninhaabc <aninhabiac123@gmail.com>
2024-08-25 14:56:27 -0300

    arrumando a versão do python

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.11.9',
+    python_requires='>=3.11',
     entry_points={
         'console_scripts': [
             'ana_patinho_vs_gatinho=jogo.tela_jogo',
